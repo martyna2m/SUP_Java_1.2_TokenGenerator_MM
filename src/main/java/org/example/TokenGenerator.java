cebeda44a582f26f63903da451ccd95a8fbdca57
martyna2m <bsom2m@gmail.com>
2023-10-06 10:53:15 +0200

    Create code for tokenGenerator

---
+++ src/main/java/org/example/TokenGenerator.java
@@ -0,0 +1,41 @@
+package org.example;
+
+import java.util.InputMismatchException;
+import java.util.Random;
+import java.util.Scanner;
+
+public class TokenGenerator {
+    Scanner scan = new Scanner(System.in);
+    Random rand = new Random();
+
+    public void generateToken() throws InputMismatchException {
+
+        welcomeToTheGame();
+        int charNumber = scan.nextInt();
+
+        String token = rand.ints(33, 123)
+
+                .filter(num -> (num != 34) && (num != 39) && (num < 43 || num > 47) && (num < 58 || num > 63) && (num < 91 || num > 93) && (num < 95 || num > 96))
+
+                .limit(charNumber)
+
+                .mapToObj(c -> (char) c).collect(StringBuffer::new, StringBuffer::append, StringBuffer::append)
+
+                .toString();
+
+        if (charNumber != 5 && charNumber != 10 && charNumber != 15) {
+            printAlert();
+        } else System.out.println("Your token: " + token);
+    }
+
+
+    private void welcomeToTheGame() {
+        System.out.println("Choose the number of characters in your token: 5, 10, or 15");
+    }
+
+    public void printAlert() {
+        System.out.println("You can only choose number 5, 10, or 15 as an option. Please start again.");
+    }
+
+}
+
